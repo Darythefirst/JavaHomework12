32f5ae34f2fba566119ae649d41b8761fa5f273e
Dary <Dafna_pelmeshka@mail.ru>
2023-05-25 19:15:35 +0300

    New tests added

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -51,5 +51,80 @@ public class TodosTest {
         Assertions.assertArrayEquals(expected, actual);
 
     }
+
+    @Test
+    public void shouldTestSearch2() {
+        SimpleTask simpleTask = new SimpleTask(19, "Записаться к врачу");
+
+        String[] subtasks = {"Купить тетради", "Купить ручки", "Купить карандаши"};
+        Epic epic = new Epic(82, subtasks);
+
+        Meeting meeting = new Meeting(
+                21,
+                "Запись к врачу",
+                "Проверить здоровье",
+                "8:45 13.07.2023"
+        );
+
+        Todos searchTodo = new Todos();
+        searchTodo.add(simpleTask);
+        searchTodo.add(epic);
+        searchTodo.add(meeting);
+
+        Task[] expected = {simpleTask, meeting};
+        Task[] actual = searchTodo.search("врач");
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldTestSearch3() {
+        SimpleTask simpleTask = new SimpleTask(1, "Записаться к парикмахеру");
+
+        String[] subtasks = {"футболка", "парик", "носки"};
+        Epic epic = new Epic(12, subtasks);
+
+        Meeting meeting = new Meeting(
+                3,
+                "Запись в салон",
+                "подстричься у парикмахера",
+                "11:00 завтра"
+        );
+
+        Todos searchTodo = new Todos();
+        searchTodo.add(simpleTask);
+        searchTodo.add(epic);
+        searchTodo.add(meeting);
+
+        Task[] expected = {simpleTask, meeting};
+        Task[] actual = searchTodo.search("парикмахер");
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldTestSearch4() {
+        SimpleTask simpleTask = new SimpleTask(10, "Встреча с Катей");
+
+        String[] subtasks = {"футболка", "шорты", "носки"};
+        Epic epic = new Epic(100, subtasks);
+
+        Meeting meeting = new Meeting(
+                120,
+                "Играем в футбол",
+                "тренировка для сборной",
+                "9:00 в воскресенье"
+        );
+
+        Todos searchTodo = new Todos();
+        searchTodo.add(simpleTask);
+        searchTodo.add(epic);
+        searchTodo.add(meeting);
+
+        Task[] expected = {epic, meeting};
+        Task[] actual = searchTodo.search("футбол");
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
 }
 
